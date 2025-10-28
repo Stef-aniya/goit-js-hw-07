6a8c8aa8e9ae2062ad284ae7683d1c9f376736ff
Stef-aniya <stefaniyamomot@gmail.com>
2025-10-28 16:43:32 +0200

    hm7

--- js/task-4.js
+++ js/task-4.js
@@ -9,13 +9,15 @@ const emailValue = email.value.trim();
 const passwordValue = password.value.trim();
 
 if(emailValue === '' || passwordValue === ''){
-alert('All form fields must be filled in')
-return
+alert('All form fields must be filled in');
+return;
 }
+
 const formData ={
     email: emailValue,
     password: passwordValue
-}
+};
+
 console.log(formData);
 
 form.reset();
